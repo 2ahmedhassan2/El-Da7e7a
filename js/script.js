49325c140288ae587e55ded932514a9528be052c
2ahmedhassan2 <111598403+2ahmedhassan2@users.noreply.github.com>
2024-12-07 14:24:56 +0200

    Create script.js

---
+++ js/script.js
@@ -0,0 +1,14 @@
+const text = "خاص فقط بالفرقة الثانية | كليه تجاره جامعة أسيوط";
+const animatedText = document.getElementById("animated-text");
+
+let index = 0;
+
+function typeText() {
+  if (index < text.length) {
+    animatedText.textContent += text.charAt(index);
+    index++;
+    setTimeout(typeText, 100); // Adjust typing speed here
+  }
+}
+
+typeText();
